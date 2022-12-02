1350534d158e626363fc73ade5cd7bce96cb09ec
JoshuaWeir <joshua.weir@ucalgary.ca>
2022-12-02 12:26:10 -0700

    Created LocalDates. Added released/early checks

--- Showtime.java
+++ Showtime.java
@@ -1,33 +1,54 @@
 import java.lang.String;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 class Showtime {
     private String movie;
     private int screen;
-    private String releaseDate; //dd-mm-yyyy
-    private String earlyAccess;
-    private int time; //hh-mm
+    private LocalDate releaseDate; //dd-mm-yyyy
+    private LocalDate earlyAccess;
+    private String time; //hh:mmam/pm
     SeatingMap seatMap;
 
-    public Showtime(int t, String m, int s, String rd){
+    public Showtime(String t, String m, int s, String rd){
         this.movie = m;
         this.screen = s;
         this.time = t;
-        this.releaseDate = rd;
+        setReleaseDate(rd);
+        setEarlyAccess(rd);
         this.seatMap = new SeatingMap();
     }
 
     //getters
     public String getMovie() { return this.movie; }
     public int getScreen() { return this.screen; }
-    public String getReleaseDate() { return this.releaseDate; }
-    public String getEarlyAccess() { return this.earlyAccess; }
-    public int getTime() {return this.time; }
-    public SeatingMap getSeatMap() { return this.seatMap; }
+    public LocalDate getReleaseDate() { return this.releaseDate; }
+    public LocalDate getEarlyAccess() { return this.earlyAccess; }
+    public String getTime() {return this.time; }
+    public SeatingMap getSeats() { return this.seatMap; }
 
     //setters
     public void setMovie(String movie){ this.movie = movie; }
     public void setScreen(int screen){ this.screen = screen; }
-    public void setReleaseDate(String rd){ this.releaseDate = rd; }
-    public void setEarlyAccess(String ea){ this.earlyAccess = ea; }
-    public void setTime(int time){ this.time = time; }
+    public void setReleaseDate(String rd){ this.releaseDate = LocalDate.parse(rd, DateTimeFormatter.ofPattern("dd-MM-yyyy")); }
+    public void setEarlyAccess(String ea){ this.earlyAccess = this.releaseDate.minusWeeks(1); }
+    public void setTime(String time){ this.time = time; }
+
+    public boolean isReleased(){
+        if (this.releaseDate.isAfter(LocalDate.now())){
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+
+    public boolean isEarly(){
+        if (this.earlyAccess.isAfter(LocalDate.now())){
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
 }
