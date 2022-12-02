f6ab2b50011ddb8df0911b0b615d5628bbf904b2
JoshuaWeir <joshua.weir@ucalgary.ca>
2022-12-02 12:02:03 -0700

    added price member and getter

--- SeatingMap.java
+++ SeatingMap.java
@@ -2,6 +2,7 @@ class SeatingMap{
     private int[][] seats;
     private boolean tenPercentSold = false; 
     private boolean soldOut = false;
+    private static double price = 10;
 
     public SeatingMap(){
         this.seats = new int[10][10];
@@ -15,6 +16,7 @@ class SeatingMap{
     public int[][] getSeats() { return this.seats; }
     public boolean getTenPercent() {return this.tenPercentSold; }
     public boolean getSoldOut() { return this.soldOut; }
+    public static double getPrice(){ return price; }
 
     public boolean checkSoldOut(){
         int count = 0;
@@ -49,6 +51,4 @@ class SeatingMap{
             return false;
         }
     }
-
-    
 }
