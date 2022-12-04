f3aa6c42fbbffba8460acf40fa67684cbf2b9919
Jgerbrandt <jgerbrandt@live.com>
2022-12-03 21:29:51 -0700

    OrderReceipt update

--- RegisteredReceipt.java
+++ RegisteredReceipt.java
@@ -1,17 +1,20 @@
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 
-
 public class RegisteredReceipt  implements Receipt {
     private Ticket ticket;
     private Registered user;
     private Order order;
+    private Showtime showtime;
 
     @Override
     public void createOrderReceipt() throws FileNotFoundException {
         PrintWriter orderReceipt = new PrintWriter("OrderReceipt.txt");
         
         orderReceipt.println("Movie: " + ticket.getMovieName());
+        orderReceipt.println("Screen: " + showtime.getScreen());
+        orderReceipt.println("Date: " + showtime.getShowDate());
+        orderReceipt.println("Time: " + showtime.getTime());
         orderReceipt.println("Email: " + user.getEmail());
         orderReceipt.println("Price: " + order.getPrice());
         orderReceipt.println("Date Purchased: " + order.getpurchaseDate());
