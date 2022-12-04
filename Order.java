90910ec6d1157a4016e3a045965812f5d49d34b6
JoshuaWeir <joshua.weir@ucalgary.ca>
2022-12-04 13:27:12 -0700

    Order functions

--- Order.java
+++ Order.java
@@ -1,21 +1,97 @@
-class Order {
+import java.time.Duration;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.time.format.DateTimeFormatter;
+
+public class Order {
     private int OrderID;
     private String email;
     private List<Ticket> tickets;
-
-
+    private double price;
+    private LocalDate purchaseDate;
+    private LocalDate refundDate; 
     public static int OrderIDCounter;
+    private boolean isPaid = false;
 
-    public Order(int id, String email) {
-        OrderID = id;
+    public Order(String email, List<Ticket> tickets) {
+        this.OrderID = OrderIDCounter;
+        OrderIDCounter++;
         this.email = email;
-        tickets = new ArrayList<Ticket>();
+        this.tickets = tickets;
+        this.price = (double) tickets.size() * 10;
+        this.refundDate = tickets.get(0).getShowtime().getShowDate().minusDays(3);
     }
 
+    public Order(int OrderID, String email, double price, String rd) {
+        this.OrderID = OrderID;
+        this.email = email;
+        this.price = price;
+        this.refundDate = LocalDate.parse(rd, DateTimeFormatter.ofPattern("dd-MM-yyyy"));
+    }
 
-    public void addTicket(int id, int col, int row. int showtimeID) {
-	    Ticket ticket = new Ticket(id,col,row,showtimeID);
+    public void addTicket(Ticket ticket) {
         tickets.add(ticket);
+    }
+
+    public int getOrderID() {
+        return this.OrderID;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public List<Ticket> getTickets() {
+        return this.tickets;
+    }
+
+    public double getPrice() {
+        return this.price;
+    }
+
+    public LocalDate getpurchaseDate() {
+        return this.purchaseDate;
+    }
+
+    public String getRefundDate() {
+        String formattedDate = this.refundDate.format(DateTimeFormatter.ofPattern("dd-MM-yyyy"));
+        return formattedDate;
+    }
+
+    public void setOrderID(int OrderID) {
+        this.OrderID = OrderID;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setTickets(List<Ticket> tickets) {
+        this.tickets = tickets;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setPurchaseDate(LocalDate purchaseDate) {
+        this.purchaseDate = purchaseDate;
+    }
+
+    public void setRefundDate(String refundDate) {
+        this.refundDate = LocalDate.parse(refundDate, DateTimeFormatter.ofPattern("dd-MM-yyyy"));
+    }
+
+    public void payForOrder(){
+        MovieDatabase movie = new MovieDatabase();
+        this.isPaid = true;
+        movie.addOrderToDB(this);    
+    }
+
+    public void cancelOrder(){
+        MovieDatabase movie = new MovieDatabase();
+
     }
 
 }
--- Ticket.java
+++ Ticket.java
@@ -1,17 +1,80 @@
-class Ticket {
-    private int TicketID;
-    private int showtimeID;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
+public class Ticket {
+    private int ticketID;
+    private String movieName;
     private int seatColumn;
     private int seatRow;
-
+    private int showtimeID;
+    private Showtime showtime;
     public static int TicketIDCounter;
 
-
-    public Ticket(int id, int col, int row, int showtimeID) {
-        this.TicketID = id;
-        this.showtimeID = showtimeID;
+    public Ticket(int id, String name, int col, int row, int showtimeID, Showtime showtime) {
+        this.ticketID = id;
+        this.movieName = name;
         this.seatColumn = col;
         this.seatRow = row;
+        this.showtimeID = showtimeID;
+        this.showtime = showtime;
     }
 
+    public void createTicket() throws FileNotFoundException {
+        PrintWriter ticket = new PrintWriter("Ticket.txt");
+
+        ticket.println("Movie: " + getMovieName());
+        ticket.println("Screen: " + showtime.getScreen());
+        ticket.println("Date: " + showtime.getShowDate());
+        ticket.println("Time: " + showtime.getTime());
+
+        ticket.close();
+    }
+
+    public int getTicketID() {
+        return this.ticketID;
+    }
+
+    public String getMovieName() {
+        return this.movieName;
+    }
+
+    public int getSeatColumn() {
+        return this.seatColumn;
+    }
+
+    public int getSeatRow() {
+        return this.seatRow;
+    }
+
+    public int getshowtimeID() {
+        return this.showtimeID;
+    }
+
+    public Showtime getShowtime() {
+        return this.showtime;
+    }
+
+    public void setTicketID(int ticketID) {
+        this.ticketID = ticketID;
+    }
+
+    public void setMovieName(String movieName) {
+        this.movieName = movieName;
+    }
+
+    public void setSeatColumn(int seatColumn) {
+        this.seatColumn = seatColumn;
+    }
+
+    public void setSeatRow(int seatRow) {
+        this.seatRow = seatRow;
+    }
+
+    public void setShowtimeID(int showtimeID) {
+        this.showtimeID = showtimeID;
+    }
+
+    public void setShowtime(Showtime showtime) {
+        this.showtime = showtime;
+    }
 }
