f02bfbc79c41e543ea51acd15208af0e3e690985
JoshuaWeir <joshua.weir@ucalgary.ca>
2022-12-03 20:09:49 -0700

    Created functional DC class. Updated MDB funcs

--- DiscountCode.java
+++ DiscountCode.java
@@ -1,15 +1,30 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+
 class DiscountCode {
+    public static int CodeIDCounter;
 	private int code;
 	private double discount;
+    private LocalDate exp;
 	
-	public DiscountCode(int code, double disc) {
-		this.code = code;
-		this.discount = disc;
-	}
-	
-	public void display(){
-		System.out.println(code);
-		System.out.println(discount);
+	public DiscountCode(double discount) {
+		this.code = CodeIDCounter;
+        CodeIDCounter++;
+		this.discount = discount;
+        this.exp = LocalDate.now().plusYears(1);
 	}
 
+    public DiscountCode(int code, double discount, String date){
+        this.code = code;
+		this.discount = discount;
+        this.exp = LocalDate.parse(date, DateTimeFormatter.ofPattern("dd-MM-yyyy"));
+    }
+	
+    public int getCode(){ return this.code; }
+    public double getDiscount(){ return this.discount; }
+    public String getExp(){ 
+        String formattedDate = this.exp.format(DateTimeFormatter.ofPattern("dd-MM-yyyy"));
+        return formattedDate;
+    }
 }
