59f4871e66eba2ed176f5b8402c59b50e9b2c0fe
Jgerbrandt <jgerbrandt@live.com>
2022-12-03 20:18:29 -0700

    OrderReceipt update

--- OrdinaryReceipt.java
+++ OrdinaryReceipt.java
@@ -4,12 +4,16 @@ import java.io.PrintWriter;
 public class OrdinaryReceipt implements Receipt {
     private Ticket ticket;
     private Order order;
-
+    private Showtime showtime;
+    
     @Override
     public void createOrderReceipt() throws FileNotFoundException {
         PrintWriter orderReceipt = new PrintWriter("OrderReceipt.txt");
         
         orderReceipt.println("Movie: " + ticket.getMovieName());
+        orderReceipt.println("Screen: " + showtime.getScreen());
+        orderReceipt.println("Date: " + showtime.getShowDate());
+        orderReceipt.println("Time: " + showtime.getTime());
         orderReceipt.println("Email: " + order.getEmail());
         orderReceipt.println("Price: " + order.getPrice());
         orderReceipt.println("Date Purchased: " + order.getpurchaseDate());
@@ -25,6 +29,7 @@ public class OrdinaryReceipt implements Receipt {
         refundReceipt.println("Email: " + order.getEmail());
         refundReceipt.println("Refund Amount: " + order.getRefundAmount());
         refundReceipt.println("Date Refunded: " + order.getRefundDate());
+        refundReceipt.println("Discount Code: " + order.getDiscountCode());
 
         refundReceipt.close();
     }
