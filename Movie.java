64fe335d11a8b22636a625ef1942b725d328e0c3
JoshuaWeir <97989830+JoshuaWeir@users.noreply.github.com>
2022-12-05 12:36:17 -0700

    Update Movie.java

--- Movie.java
+++ Movie.java
@@ -1,3 +1,8 @@
+/**
+* Movie object stroes movie data from DB
+* Composed of showtimes, and contains attributes regarding release information
+*/
+
 import java.lang.String;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
@@ -9,25 +14,36 @@ public class Movie {
     private LocalDate releaseDate; //dd-mm-yyyy
     private LocalDate earlyAccess;
     private List<Showtime> showtimes = new ArrayList<Showtime>();
-
+	
+	/**
+	* ctor to re-init object from the DB
+	* @param title		title of movie
+	* @param rd 		date movie is set to release in theatres
+	* 
+	* sets earlyAcess date to one week before movie's release date
+	*/
     public Movie(String title, String rd){
         this.title = title;
         setReleaseDate(rd);
         setEarlyAccess();
     }
-
+	
+	//getters
     public String getTitle() { return this.title; }
     public LocalDate getReleaseDate() { return this.releaseDate; }
     public LocalDate getEarlyAccess() { return this.earlyAccess; }
     public Showtime getShowtime(int index){ return this.showtimes.get(index);}
     public List<Showtime> getShowtimes(){ return this.showtimes; }
-
+	
+	//setters
     public void setTitle(String t){ this.title = t; }
     public void setReleaseDate(String rd){ this.releaseDate = LocalDate.parse(rd, DateTimeFormatter.ofPattern("dd-MM-yyyy")); }
     //early access date is one week before release
     public void setEarlyAccess(){ this.earlyAccess = this.releaseDate.minusWeeks(1); }
     
-
+	/**
+	@return whether movie has been released
+	*/
     public boolean isReleased(){
         if (this.releaseDate.isAfter(LocalDate.now())){
             return false;
@@ -37,6 +53,9 @@ public class Movie {
         }
     }
 
+	/**
+	@return whether movie is in early access period
+	*/
     public boolean isEarly(){
         if (this.earlyAccess.isAfter(LocalDate.now())){
             return false;
@@ -45,7 +64,16 @@ public class Movie {
             return true;
         }
     }
-
+	
+	/**
+	*Add's showtime to existing movie object
+	*
+	*@param id	unique showtime ID
+	@param screen	screen movie is playing on for showtime
+	@param date	date fo showtime
+	@param time	time of showtime
+	@param rd	release date of movie playing at showtime
+	*/
     public void addShowtime(int id, int screen, String date, String time, LocalDate rd){
         Showtime newShow = new Showtime(id, screen, date, time, rd);
         this.showtimes.add(newShow);
